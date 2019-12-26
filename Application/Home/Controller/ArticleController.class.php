a5680f635ce56545bd0c2c47c28bd32a7b434540
MierX <1214915547@qq.com>
2019-12-26 21:18:33 +0800

    初始化

---
+++ Application/Home/Controller/ArticleController.class.php
@@ -0,0 +1,20 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+use Think\Db;
+
+class ArticleController extends BaseController
+{
+    public function index()
+    {
+
+    }
+
+    public function list()
+    {
+        parent::special();
+        parent::list();
+        $this->display();
+    }
+}
